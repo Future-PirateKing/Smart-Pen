dc2309191ad164938e89da9977c7bf16fea7dfab
Sam David <samd948@gmail.com>
2018-02-03 21:03:11 +0530

    Completed tabs, sidenav and main layout

--- smart-pen-desktop/src/app/app.module.ts
+++ smart-pen-desktop/src/app/app.module.ts
@@ -1,5 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSidenavModule } from '@angular/material/sidenav';
 
 
 import { AppComponent } from './app.component';
@@ -14,7 +17,10 @@ import { MainAreaComponent } from './main-area/main-area.component';
     MainAreaComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatTooltipModule,
+    MatSidenavModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- smart-pen-desktop/src/app/main-area/main-area.component.ts
+++ smart-pen-desktop/src/app/main-area/main-area.component.ts
@@ -7,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MainAreaComponent implements OnInit {
 
+  colors = [
+    'red', 'pink', 'purple', 'indigo', 'blue', 'cyan', 'teal', 'green',
+    'lime', 'yellow', 'amber', 'orange', 'brown', 'grey', 'white', 'black'
+  ];
+
   constructor() { }
 
   ngOnInit() {
